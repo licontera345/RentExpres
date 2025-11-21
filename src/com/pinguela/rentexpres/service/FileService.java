8383ecb8211b14e682d75d6a66583416a68a41f3
licontera345 <liconterapelopo@gmail.com>
2025-11-21 12:15:40 +0100

    Remove REST file resource from middleware

--- src/com/pinguela/rentexpres/service/FileService.java
+++ src/com/pinguela/rentexpres/service/FileService.java
@@ -1,54 +1,24 @@
 package com.pinguela.rentexpres.service;
 
-import java.io.File;
 import java.util.List;
 
 import com.pinguela.rentexpres.exception.RentexpresException;
 
 public interface FileService {
 
-    // VEHICLE (galería: varias imágenes numeradas 1.jpg, 2.jpg, ...)
+    List<String> listVehicleImages(Integer vehicleId) throws RentexpresException;
 
-    List<File> getImagesByVehicleId(Integer vehicleId) throws RentexpresException;
+    byte[] getVehicleImage(Integer vehicleId, String imageName) throws RentexpresException;
 
-    /**
-     * Sincroniza la galería del vehículo:
-     * - Crea la carpeta si no existe.
-     * - Borra ficheros que ya no estén en "imagenes".
-     * - Copia/actualiza los ficheros de "imagenes" (añade nuevos con nombre secuencial).
-     */
-    void uploadImagesByVehicleId(List<File> imagenes, Integer vehicleId) throws RentexpresException;
+    void saveVehicleImage(Integer vehicleId, String imageName, byte[] data) throws RentexpresException;
 
+    void deleteVehicleImage(Integer vehicleId, String imageName) throws RentexpresException;
 
-    // USER (avatar único: profile.jpg)
+    byte[] getUserAvatar(Integer userId) throws RentexpresException;
 
-    File getImageByUserId(Integer userId) throws RentexpresException;
+    void saveUserAvatar(Integer userId, byte[] data) throws RentexpresException;
 
-    /**
-     * Obtiene la URL pública del avatar de un usuario concreto.
-     */
-    String getUserAvatarUrl(Integer userId);
+    byte[] getEmployeeAvatar(Integer employeeId) throws RentexpresException;
 
-    /**
-     * Guarda avatar del usuario como profile.jpg. Si es nulo o inváido,
-     * usa la imagen por defecto configurada o de classpath.
-     */
-    void uploadImageByUserId(File imagen, Integer userId) throws RentexpresException;
-
-
-    // EMPLOYEE (avatar único: profile.jpg)
-
-    File getImageByEmployeeId(Integer employeeId) throws RentexpresException;
-
-    void uploadImageByEmployeeId(File imagen, Integer employeeId) throws RentexpresException;
-
-    /**
-     * Obtiene la URL pública del avatar de un empleado concreto.
-     */
-    String getEmployeeAvatarUrl(Integer employeeId);
-
-    /**
-     * Construye la URL pública de una imagen de vehículo específica.
-     */
-    String getImageUrl(Integer vehicleId, String imageName);
+    void saveEmployeeAvatar(Integer employeeId, byte[] data) throws RentexpresException;
 }
